e1c5e8a93b30222b4b7ef7e433487ea06fba733e
Pavlo-Ivanochok <ivanochokpavlo2@gmail.com>
2024-12-20 16:07:52 +0200

    tests added

---
+++ tests/test_profile_management.py
@@ -0,0 +1,154 @@
+import allure
+import random
+from base.base_test import BaseTest
+from utils.fake_data_generator import FakeDataGenerator
+
+
+class TestProfileManagement(BaseTest):
+
+    @allure.feature("Profile Management")
+    @allure.description("""
+    This test verifies that a user can successfully delete their account by accessing the account settings and following the deletion process.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("TC07: Successful account deletion.")
+    def test_accound_deletion(self, create_account):
+        email, password, username = create_account
+        # Steps:
+        self.home_page.click_username_button()
+        self.home_page.click_settings_button()
+        self.account_management_page.click_delete_account_button()
+        self.delete_management_page.is_opened()
+        self.delete_management_page.enter_email(email)
+        self.delete_management_page.enter_password(password)
+        self.delete_management_page.click_delete_account_button()
+        self.account_deleted_page.is_opened()
+        self.account_deleted_page.is_page_heading_account_deleted()
+
+    @allure.feature("Profile Management")
+    @allure.description("""
+    This test verifies that a user can successfully change their password through the account settings by entering the current password and a new one.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("TC05: Successful password change.")
+    def test_password_change(self, create_account, delete_account):
+        email, password, username = create_account
+        fake = FakeDataGenerator()
+        new_password = fake.password
+        is_password_changed = False
+        # Steps:
+        try:
+            self.home_page.click_username_button()
+            self.home_page.click_settings_button()
+            self.account_management_page.click_change_password_button()
+            self.password_management_page.is_opened()
+            self.password_management_page.enter_password(password)
+            self.password_management_page.enter_new_password(new_password)
+            self.password_management_page.click_change_password_button()
+            self.password_management_page.is_error_message_not_visible()
+            self.account_management_page.is_opened()
+            is_password_changed = True
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created account."):
+                password_to_use = new_password if is_password_changed else password
+                delete_account(email, password_to_use)
+
+    @allure.feature("Profile Management")
+    @allure.description("""
+    This test verifies that a user can successfully change their email address and username through the account settings.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC06: Successful email and username change.")
+    def test_email_and_username_change(self, create_account, delete_account):
+        email, password, username = create_account
+        fake = FakeDataGenerator()
+        new_email = fake.email
+        new_username = fake.username
+        is_email_changed = False
+        # Steps:
+        try:
+            self.home_page.click_username_button()
+            self.home_page.click_settings_button()
+            self.account_management_page.click_change_email_button()
+            self.email_management_page.is_opened()
+            self.email_management_page.enter_new_email(new_email)
+            self.email_management_page.enter_password(password)
+            self.email_management_page.click_change_email_button()
+            self.email_management_page.is_error_message_not_visible()
+            self.account_management_page.is_opened()
+            is_email_changed = True
+            self.account_management_page.enter_new_username(new_username)
+            self.account_management_page.click_update_button()
+            self.account_management_page.click_close_settings_button()
+            self.home_page.is_sidebar_contains_username(new_username)
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created account."):
+                email_to_use = new_email if is_email_changed else email
+                delete_account(email_to_use, password)
+
+    @allure.feature("Profile Management")
+    @allure.description("""
+    This test verifies that a user cannot change the current password when providing an incorrect current password.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC10: Inability to change the current password with an incorrect password.")
+    def test_unsuccessful_password_change(self, create_account, delete_account):
+        email, password, username = create_account
+        fake = FakeDataGenerator()
+        new_password = fake.password
+        invalid_password = fake.password
+        invalid_new_password = str(random.randint(1000000, 9999999))
+        # Steps:
+        try:
+            self.home_page.click_username_button()
+            self.home_page.click_settings_button()
+            self.account_management_page.click_change_password_button()
+            self.password_management_page.is_opened()
+            self.password_management_page.enter_password(invalid_password)
+            self.password_management_page.enter_new_password(new_password)
+            self.password_management_page.click_change_password_button()
+            self.password_management_page.is_error_message_visible()
+            self.password_management_page.is_opened()
+            self.password_management_page.enter_password(password)
+            self.password_management_page.enter_new_password(invalid_new_password)
+            self.password_management_page.click_change_password_button()
+            self.password_management_page.is_error_message_visible()
+            self.password_management_page.is_opened()
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created account."):
+                delete_account(email, password)
+
+    @allure.feature("Profile Management")
+    @allure.description("""
+    This test verifies that a user cannot delete their account when providing an incorrect email or password.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC11: Inability to delete the account with an incorrect email or password.")
+    def test_unsuccessful_account_deletion(self, create_account, delete_account):
+        email, password, username = create_account
+        fake = FakeDataGenerator()
+        invalid_email = fake.email
+        invalid_password = fake.password
+        # Steps:
+        try:
+            self.home_page.click_username_button()
+            self.home_page.click_settings_button()
+            self.account_management_page.click_delete_account_button()
+            self.delete_management_page.is_opened()
+            self.delete_management_page.enter_email(email)
+            self.delete_management_page.enter_password(invalid_password)
+            self.delete_management_page.click_delete_account_button()
+            self.delete_management_page.is_error_message_visible()
+            self.delete_management_page.is_opened()
+            self.delete_management_page.enter_email(invalid_email)
+            self.delete_management_page.enter_password(password)
+            self.delete_management_page.click_delete_account_button()
+            self.delete_management_page.is_error_message_visible()
+            self.delete_management_page.is_opened()
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created account."):
+                delete_account(email, password)
---
+++ tests/test_projects_management.py
@@ -0,0 +1,198 @@
+import allure
+from base.base_test import BaseTest
+from utils.fake_data_generator import FakeDataGenerator
+
+
+class TestProjectsManagement(BaseTest):
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a new Task List can be successfully created with a valid name.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("TC23: Successful creation of a new Task List.")
+    def test_task_list_creation(self, login, delete_project):
+        fake = FakeDataGenerator()
+        project_name = fake.project_name
+        # Steps:
+        self.home_page.click_my_projects_button()
+        self.projects_page.is_opened()
+        self.projects_page.click_my_projects_menu_button()
+        self.projects_page.click_add_project_button()
+        self.projects_page.enter_project_name(project_name)
+        self.projects_page.choose_list()
+        self.projects_page.click_add_button()
+        self.projects_page.is_my_projects_section_contains_project(project_name)
+        # Postconditions:
+        with allure.step("Postconditions: Delete the created project."):
+            delete_project(project_name)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a new Task Board can be successfully created with multiple sections.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("TC24: Successful creation of a new Task Board with sections.")
+    def test_task_board_creation(self, login, delete_project):
+        fake = FakeDataGenerator()
+        project_name = fake.project_name
+        first_section_name = "To Do"
+        second_section_name = "In Progress"
+        third_section_name = "Done"
+        # Steps:
+        try:
+            self.home_page.click_my_projects_button()
+            self.projects_page.is_opened()
+            self.projects_page.click_my_projects_menu_button()
+            self.projects_page.click_add_project_button()
+            self.projects_page.enter_project_name(project_name)
+            self.projects_page.choose_board()
+            self.projects_page.click_add_button()
+            self.projects_page.is_my_projects_section_contains_project(project_name)
+            self.projects_page.is_board_sections_visible()
+            self.projects_page.enter_section_name(first_section_name)
+            self.projects_page.click_confirm_add_section_button()
+            self.projects_page.click_add_section_button()
+            self.projects_page.enter_section_name(second_section_name)
+            self.projects_page.click_confirm_add_section_button()
+            self.projects_page.click_add_section_button()
+            self.projects_page.enter_section_name(third_section_name)
+            self.projects_page.click_confirm_add_section_button()
+            self.projects_page.is_board_contains_section(first_section_name)
+            self.projects_page.is_board_contains_section(second_section_name)
+            self.projects_page.is_board_contains_section(third_section_name)
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created project."):
+                delete_project(project_name)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that the project name and type can be successfully changed.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC25: Successful change of the Project name and type.")
+    def test_project_name_type_change(self, login, create_task_list, delete_project):
+        project_name = create_task_list
+        fake = FakeDataGenerator()
+        new_project_name = fake.project_name
+        is_project_name_changed = False
+        # Steps:
+        try:
+            self.projects_page.click_more_actions_button()
+            self.projects_page.click_edit_button()
+            self.projects_page.enter_project_name(new_project_name)
+            self.projects_page.choose_board()
+            self.projects_page.click_save_button()
+            self.projects_page.is_my_projects_section_contains_project(new_project_name)
+            is_project_name_changed = True
+            self.projects_page.is_board_sections_visible()
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created project."):
+                project_name_to_use = new_project_name if is_project_name_changed else project_name
+                delete_project(project_name_to_use)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a user can successfully delete an existing project.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("TC26: Successful deletion of a Project.")
+    def test_project_deletion(self, login, create_task_list):
+        project_name = create_task_list
+        # Steps:
+        self.home_page.click_my_projects_button()
+        self.projects_page.open_project(project_name)
+        self.projects_page.click_more_actions_button()
+        self.projects_page.click_delete_button()
+        self.projects_page.is_my_project_section_not_contains_project(project_name)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that the system prevents the creation of a new project when invalid information is provided.
+    """)
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.title("TC27: Unsuccessful creation of a new Project due to invalid information.")
+    def test_unsuccessful_project_creation(self, login):
+        project_name = ""
+        # Steps:
+        self.home_page.click_my_projects_button()
+        self.projects_page.click_my_projects_menu_button()
+        self.projects_page.click_add_project_button()
+        self.projects_page.enter_project_name(project_name)
+        self.projects_page.choose_list()
+        self.projects_page.is_add_button_disabled()
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a user can successfully duplicate an existing task list, creating an identical copy.
+    """)
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.title("TC28: Successful duplication of a Task List.")
+    def test_task_list_duplication(self, login, create_task_list, create_task, delete_project):
+        project_name = create_task_list
+        task_name, task_description, task_priority = create_task
+        # Steps:
+        try:
+            self.projects_page.click_more_actions_button()
+            self.projects_page.click_duplicate_button()
+            self.projects_page.is_my_projects_section_contains_project("Copy of " + project_name)
+            self.projects_page.click_my_projects_button()
+            self.projects_page.open_project("Copy of " + project_name)
+            self.projects_page.is_task_list_contains_task(task_name)
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created project and its duplicate."):
+                delete_project(project_name)
+                delete_project("Copy of " + project_name)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a user can successfully archive a project, confirm its archival in the archived projects section, and restore it back to the active projects section.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC29: Successful archiving of a Project.")
+    def test_project_archiving(self, login, create_task_list, delete_project):
+        project_name = create_task_list
+        # Steps:
+        try:
+            self.projects_page.click_more_actions_button()
+            self.projects_page.click_archive_button()
+            self.projects_page.is_my_project_section_not_contains_project(project_name)
+            self.projects_page.click_my_projects_button()
+            self.projects_page.click_active_projects_button()
+            self.projects_page.click_archived_projects_button()
+            self.projects_page.is_archived_section_contains_project(project_name)
+            self.projects_page.open_project(project_name)
+            self.projects_page.is_archived_message_visible()
+            self.projects_page.click_unarchive_button()
+            self.projects_page.is_my_projects_section_contains_project(project_name)
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created project."):
+                delete_project(project_name)
+
+    @allure.feature("Projects Management")
+    @allure.description("""
+    This test verifies that a user can successfully move a task from one column to another on the Board, ensuring the task's position is updated correctly and persists after a page refresh.
+    """)
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.title("TC30: Successful task moving on Board.")
+    def test_task_moving_on_board(self, login, create_task_board, delete_project):
+        project_name, first_section, second_section, third_section = create_task_board
+        fake = FakeDataGenerator()
+        task_name = fake.task_name
+        # Steps:
+        try:
+            self.projects_page.create_task_in_section(first_section, task_name)
+            self.projects_page.click_more_actions_button_on_task(task_name)
+            self.projects_page.move_task_to_section(second_section, project_name)
+            self.projects_page.is_section_contains_task(task_name, second_section)
+            self.projects_page.click_more_actions_button_on_task(task_name)
+            self.projects_page.move_task_to_section(third_section, project_name)
+            self.projects_page.is_section_contains_task(task_name, third_section)
+        # Postconditions:
+        finally:
+            with allure.step("Postconditions: Delete the created project."):
+                delete_project(project_name)
